85a7f3b34f400dd4515f9c476ac83750631df9fd
tanver68 <16101068@uap-bd.edu>
2020-12-20 02:45:36 +0600

    multiple data added and eventHandlar added

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ function App() {
     .then(data =>setCountries(data)) //akhane 250 countries er data gulo countries er vitor set kora hoyce .r set korar jonno setCountries k call kore tar vitor data tak pathay delam
     .catch(error => console.log(error))
   },[])
+
+  const handleAddCountry = (country) => {console.log('added',country)} //country.js j information ashlo ta amra (country) ay parameter e  recive kore console kore show korlam
   return (
     <div className="App">
       <header className="App-header">
       <h1>Countri loaded:{countries.length}</h1> 
       {
-        countries.map(country => <Country name={country.name} ></Country>)// child componants: akhane <Country></Country> hoche child components ,karon data ace app .js er vitor r amra app.js er vitor Country component use kortaci tay ata child componants 
+        countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code} ></Country>)// child componants: akhane <Country></Country> hoche child components ,karon data ace app .js er vitor r amra app.js er vitor Country component use kortaci tay ata child componants 
       }
        
         
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Country.css';
 
 const Country = (props) => {
+    const {name,population,region,flag} = props.country
+    const handleAddCountry = props.handleAddCountry;
     return (
-        <div>
-            <h2>This is : {props.name} </h2>
+        <div className="country">
+            <h2>This is : {name} </h2>
+            <img src={flag} alt=""/>
+            <p>Here Population : {population}</p>
+            <p><small>This Region : {region}</small></p>
+            <button onClick={ () => handleAddCountry(props.country)} >Add button</button> 
+            
+            {/* click korar pore amra country take app.js e pathachi ,akhn app.js e giya recive korte hobe  */}
         </div>
     );
 };
