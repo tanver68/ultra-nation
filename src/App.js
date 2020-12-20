11e411a775899c49053eacf8b6460d2bc814cbf0
tanver68 <16101068@uap-bd.edu>
2020-12-20 15:17:57 +0600

    added newcart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Country from './Components/Country/Country';
+import Cart from './Components/Cart/Cart';
 
 function App() {
-  const [countries,setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
@@ -12,11 +14,22 @@ function App() {
     .catch(error => console.log(error))
   },[])
 
-  const handleAddCountry = (country) => {console.log('added',country)} //country.js j information ashlo ta amra (country) ay parameter e  recive kore console kore show korlam
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country]; 
+                //akhane ...cart :mane copy korlam ,copy kore bosiye fdelam ,ay line er mane purangulo boshlo r notun (country) takeo bosai delam
+              //country.js j information ashlo ta amra (country) ay parameter e  recive kore console kore show korlam
+    setCart(newCart);
+  }
+  
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
       <h1>Countri loaded:{countries.length}</h1> 
+      <h4>Country Added :{cart.length} </h4>
+      <Cart cart={cart}></Cart>
       {
         countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code} ></Country>)// child componants: akhane <Country></Country> hoche child components ,karon data ace app .js er vitor r amra app.js er vitor Country component use kortaci tay ata child componants 
       }
